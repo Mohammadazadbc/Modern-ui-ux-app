ae43bbd7d5c42c03d69a5be747a52ee8e2ee8a18
mohammad azad <mohammadazad.bc@gmail.com>
2022-09-04 13:39:19 +0200

    bring in top

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -21,7 +21,7 @@ function Billing() {
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
         Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio
         aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea
-        placerat.
+        placerat
       </p>
 
       <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
